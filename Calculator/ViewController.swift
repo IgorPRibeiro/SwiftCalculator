27b27d8f2e328ec3e157a74b14d5893d5204f723
Igor Pinheiro Ribeiro <igor.ribeiro@globalsys.com.br>
2025-09-13 14:23:56 -0300

    accumalating number

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Angela Yu on 10/09/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    private var isFinishTypingNumber: Bool = true
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a non-number button is pressed
+        isFinishTypingNumber = true
+        
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Cannot convert display label text to a Double.")
+        }
+        
+    
+    }
+
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a number is entered into the keypad
+        
+        if let numValue = sender.currentTitle {
+            
+            if isFinishTypingNumber {
+                displayLabel.text = numValue
+                isFinishTypingNumber = false
+            }else  {
+                displayLabel.text = displayLabel.text! + numValue
+
+            }
+        }
+    
+    }
+
+}
+

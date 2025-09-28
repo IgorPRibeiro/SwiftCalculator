539044242211b69b55c2977d49641b9f6ad855ef
Igor Pinheiro Ribeiro <igor.ribeiro@globalsys.com.br>
2025-09-28 13:30:04 -0300

    Struct vs Classes; Tuple

---
+++ Calculator/Model/CalculatorLogic.swift
@@ -0,0 +1,9 @@
+//
+//  CalculatorLogic.swift
+//  Calculator
+//
+//  Created by Igor Pinheiro Ribeiro on 21/09/25.
+//  Copyright © 2025 London App Brewery. All rights reserved.
+//
+
+import Foundation
